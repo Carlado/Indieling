0730eaabbf224c5e0b20f82fd9dd8c5866107138
Carlado <mikael_adolfsson@live.se>
2016-12-06 15:28:17 +0100

    add search route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import Search from './containers/search';
 
 
 export default (
   <Route path="/" component={App}>
-
+    <Route path="/search" component={Search} />
   </Route>
 );
