026736aa32a86c6773c612a5eb02bc4ec1dcd8e4
Carlado <mikael_adolfsson@live.se>
2016-12-06 15:27:48 +0100

    add topbar

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const Topbar = () => {
   return (
     <div className="topbar">
       Indieling
+      <FontAwesome
+        name="search"
+        size="lg"
+      />
     </div>
   );
 };
