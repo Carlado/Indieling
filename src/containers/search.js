257986c138ca9cbea67a370aafa366181fbaa204
Carlado <mikael_adolfsson@live.se>
2016-12-06 15:27:56 +0100

    add search page

---
+++ src/containers/search.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import SearchBar from './searchbar';
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
