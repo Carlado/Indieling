e938a46e8c919730aa3a6011973e0a4efc952650
Carlado <mikael_adolfsson@live.se>
2016-12-08 15:53:01 +0100

    set active state and effect on navbar

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
+import { Link, IndexLink } from 'react-router';
 
 const Topbar = () => {
   return (
     <div className="topbar">
-      Indieling
-      <FontAwesome
-        name="search"
-        size="lg"
-      />
+      <ul className="nav-items">
+        <li><IndexLink to="/" activeClassName="active"><FontAwesome className="nav-icon" name="home" size="lg"/>Indieling</IndexLink></li>
+        <li><Link to="/search" activeClassName="active"><FontAwesome className="nav-icon" name="search" size="lg"/>Search</Link></li>
+      </ul>
+
     </div>
   );
 };
