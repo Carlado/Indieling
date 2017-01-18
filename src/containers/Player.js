cfc6efd51e4c40a51e0d0dae2b31fbf0cd85f80b
Carlado <mikael_adolfsson@live.se>
2017-01-18 19:21:17 +0100

    add volume control to player

--- src/containers/Player.js
+++ src/containers/Player.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Sound from 'react-sound';
 import {connect} from 'react-redux';
+import InputRange from 'react-input-range';
+
 
 class Player extends Component {
 
@@ -9,49 +11,61 @@ class Player extends Component {
     this.onPlay = this.onPlay.bind(this);
     this.onPause = this.onPause.bind(this);
     this.setVolume = this.setVolume.bind(this);
-    this.state = {currentSong: '',
-                  playStatus: Sound.status.PAUSED,
+    this.state = {playStatus: Sound.status.PLAYING,
                   position: 0,
-                  volume: 100};
-  }
-
-
-
-
-  setCurrentTrack() {
-    const {track} = this.props;
-    this.setState({currentSong: track.audio});
+                  volume: 50};
   }
 
   onPlay() {
-    this.setCurrentTrack();
     this.setState({playStatus: Sound.status.PLAYING});
   }
 
   onPause() {
     this.setState({playStatus: Sound.status.PAUSED});
   }
-  setVolume(event) {
-    this.setState({volume: Number(event.target.value)});
+  setVolume(component, value) {
+    this.setState({volume: value});
   }
 
   render() {
+    if (!this.props.track) {
+      return <div />
+    }
+
+    const {track} = this.props;
+
     return (
       <div className="player">
         {this.state.playStatus !== Sound.status.PLAYING ?
           <button className="player-play" onClick={this.onPlay}>Play</button> :
           <button className="player-play" onClick={this.onPause}>Pause</button>
         }
-        <input type="range" name="volume" min="0" max="100" value={this.state.volume} onChange={this.setVolume}/>
-        <p style={{color: "black"}}>{this.props.track ? this.props.track.name : null}</p>
+        <div>
+          <InputRange
+          maxValue={100}
+          minValue={0}
+          value={this.state.volume}
+          onChange={this.setVolume} />
+        </div>
+        <div>
+          <InputRange
+          maxValue={track.duration}
+          minValue={0}
+          value={this.state.position / 1000}
+           />
+        </div>
+        <p style={{color: "black"}}>{track.name}</p>
 
         <Sound
-        url={this.state.currentSong}
+        url={track.audio}
         playStatus={this.state.playStatus}
         playFromPosition={this.state.position}
         volume={this.state.volume}
+        onPlaying={({position}) => this.setState({position})}
         onFinishedPlaying={() => this.setState({playStatus: Sound.status.STOPPED})}
         />
+
+
       </div>
     );
   }
